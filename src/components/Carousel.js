496d0213c7cdae3a28d3d06958529849e7495042
Troy Washington <Troy/Washington1@AlaskaAir.com>
2022-12-14 21:54:09 -0800

    pause effect on carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 import "./Carousel.css";
 
@@ -12,19 +12,38 @@ export const CarouselItem = ({ children, width }) => {
 
 const Carousel = ({ children }) => {
     const [activeIndex, setActiveIndex] = useState(0);
+    const [paused, setPaused] = useState(false);
 
     const updateIndex = (newIndex) => {
         if (newIndex < 0) {
-            newIndex = 0;
-        } else if (newIndex >= React.Children.count(children)) {
             newIndex = React.Children.count(children) -1;
+        } else if (newIndex >= React.Children.count(children)) {
+            newIndex = 0;
         }
-
+    
         setActiveIndex(newIndex);
-    }
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (!paused) {
+                updateIndex(activeIndex + 1);
+            }
+        }, 1000);
+
+        return () => {
+            if (interval) {
+                clearInterval(interval);
+            }
+        };
+    });
 
     return (
-        <div className="carousel">
+        <div 
+            className="carousel"
+            onMouseEnter={() => setPaused(true)}
+            onMouseLeave={() => setPaused(false)}
+        >
             <div
                 className="inner"
                 style={{ transform: `translateX(-${activeIndex * 100}%)` }}
